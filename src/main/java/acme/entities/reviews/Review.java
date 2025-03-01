1232ac3fb8d1f0a99ffdf83137bbb3c43ca5b53d
mansangar13 <mansangar13@alum.us.es>
2025-03-01 21:14:39 +0100

    Groupal-Task-022 Modelo de Review
    Aún falta la relación

---
+++ src/main/java/acme/entities/reviews/Review.java
@@ -0,0 +1,65 @@
+
+package acme.entities.reviews;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity()
+@Getter()
+@Setter()
+public class Review extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory()
+	@ValidString(max = 50)
+	@Automapped()
+	private String				name;
+
+	@Mandatory()
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Mandatory()
+	@ValidString(max = 50)
+	@Automapped()
+	private String				subject;
+
+	@Mandatory()
+	@ValidString()
+	@Automapped()
+	private String				text;
+
+	@Optional()
+	@ValidNumber(min = 0, max = 10, integer = 2, fraction = 1)
+	private Double				score;
+
+	@Mandatory()
+	@Valid()
+	@Automapped()
+	private Boolean				isRecommended;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
