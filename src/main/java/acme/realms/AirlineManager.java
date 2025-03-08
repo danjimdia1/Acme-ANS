fe4432b7b345a61dd2cae816e1ca0ff178cea0c8
mansangar13 <mansangar13@alum.us.es>
2025-03-08 14:57:12 +0100

    Añadido validador custom de AirlineManager

--- src/main/java/acme/realms/AirlineManager.java
+++ src/main/java/acme/realms/AirlineManager.java
@@ -16,12 +16,14 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidAirlineManager;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidAirlineManager
 public class AirlineManager extends AbstractRole {
 
 	// Serialisation version --------------------------------------------------
