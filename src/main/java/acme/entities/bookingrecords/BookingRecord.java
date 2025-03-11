aab63f35e641af8b297d6f189e3858601d875bef
danjimdia1 <danjimdia1@alum.us.es>
2025-03-11 16:17:17 +0100

    Student#2-TAsk-013

---
+++ src/main/java/acme/entities/bookingrecords/BookingRecord.java
@@ -0,0 +1,29 @@
+
+package acme.entities.bookingrecords;
+
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.entities.bookings.Booking;
+import acme.entities.passengers.Passenger;
+
+public class BookingRecord extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Booking				booking;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Passenger			passenger;
+}
