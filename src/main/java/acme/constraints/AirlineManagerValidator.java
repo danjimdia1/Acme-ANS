917e420fcc6f3aef67e01563624fe493cf30814d
mansangar13 <mansangar13@alum.us.es>
2025-03-11 19:00:59 +0100

    Fix identifier validator

--- src/main/java/acme/constraints/AirlineManagerValidator.java
+++ src/main/java/acme/constraints/AirlineManagerValidator.java
@@ -25,25 +25,23 @@ public class AirlineManagerValidator extends AbstractValidator<ValidAirlineManag
 		if (airlineManager == null)
 			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
 		else {
-			String fullName = airlineManager.getUserAccount().getIdentity().getFullName();
-			String[] nameParts = fullName.split(", ");
+
 			String initials = "";
+			String name = airlineManager.getUserAccount().getIdentity().getName();
+			String surname = airlineManager.getUserAccount().getIdentity().getSurname();
 
-			String[] surnameParts = nameParts[0].split(" ");
-			initials = nameParts[1].substring(0, 1).toUpperCase();
-			initials += surnameParts[0].substring(0, 1).toUpperCase();
-
-			if (surnameParts.length > 1)
-				initials += surnameParts[1].substring(0, 1).toUpperCase();
+			initials += name.charAt(0);
+			initials += surname.charAt(0);
 
 			boolean validIdentifier;
 
 			String identifier = airlineManager.getIdentifier();
 
 			boolean validLength = identifier.length() >= 8 && identifier.length() <= 9;
-			boolean validPattern = identifier.matches("^" + initials + "\\d{6}$");
 
-			validIdentifier = validLength && validPattern;
+			boolean validInitials = identifier.startsWith(initials);
+
+			validIdentifier = validLength && validInitials;
 
 			super.state(context, validIdentifier, "identifier", "java.validation.airlineManager.identifier.message");
 		}
