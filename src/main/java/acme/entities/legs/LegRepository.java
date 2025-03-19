85ce6ae7030e48a4e67979b251b435526dd61c83
mansangar13 <mansangar13@alum.us.es>
2025-03-19 20:24:51 +0100

    Añadido arreglos para Flight

--- src/main/java/acme/entities/legs/LegRepository.java
+++ src/main/java/acme/entities/legs/LegRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -15,20 +14,20 @@ import acme.client.repositories.AbstractRepository;
 public interface LegRepository extends AbstractRepository {
 
 	@Query("SELECT l FROM Leg l WHERE l.flightNumber = :flightNumber AND l.id != :legId")
-	Optional<Leg> findByFlightNumber(@Param("flightNumber") String flightNumber, @Param("legId") int legId);
+	Optional<Leg> findByFlightNumber(String flightNumber, int legId);
 
 	@Query("SELECT COUNT(l) from Leg l WHERE l.flight.id = :flightId")
-	Integer numberOfLavoyers(@Param("flightId") int flightId);
+	Integer numberOfLavoyers(int flightId);
 
 	@Query("SELECT l.scheduledDeparture FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC")
-	List<Date> findFirstScheduledDeparture(@Param("flightId") int flightId);
+	List<Date> findFirstScheduledDeparture(int flightId);
 
 	@Query("SELECT l.scheduledArrival FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC")
-	List<Date> findLastScheduledArrival(@Param("flightId") int flightId);
+	List<Date> findLastScheduledArrival(int flightId);
 
 	@Query("SELECT l.departureAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledDeparture ASC")
-	List<String> findFirstOriginCity(@Param("flightId") int flightId);
+	List<String> findFirstOriginCity(int flightId);
 
 	@Query("SELECT l.arrivalAirport.city FROM Leg l WHERE l.flight.id = :flightId ORDER BY l.scheduledArrival DESC")
-	List<String> findLastDestinationCity(@Param("flightId") int flightId);
+	List<String> findLastDestinationCity(int flightId);
 }
