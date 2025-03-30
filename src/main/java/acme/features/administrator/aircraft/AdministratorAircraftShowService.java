633163fd13764689f6e7c037644048038baedf6b
danjimdia1 <danjimdia1@alum.us.es>
2025-03-30 12:40:31 +0200

    Groupal-Task-036

---
+++ src/main/java/acme/features/administrator/aircraft/AdministratorAircraftShowService.java
@@ -0,0 +1,65 @@
+
+package acme.features.administrator.aircraft;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.aircraft.Aircraft;
+import acme.entities.aircraft.Status;
+import acme.entities.airlines.Airline;
+
+@GuiService
+public class AdministratorAircraftShowService extends AbstractGuiService<Administrator, Aircraft> {
+
+	// Internal state -------------------------------------------------------------------
+	@Autowired
+	private AdministratorAircraftRepository repository;
+
+	// AbstractGuiService interface -----------------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Aircraft aircraft;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		aircraft = this.repository.findAircraftById(id);
+
+		super.getBuffer().addData(aircraft);
+	}
+
+	@Override
+	public void unbind(final Aircraft aircraft) {
+		SelectChoices statuses;
+		Dataset dataset;
+		SelectChoices selectedAirlines;
+		Collection<Airline> airlines;
+
+		airlines = this.repository.findAllAirlines();
+		selectedAirlines = SelectChoices.from(airlines, "name", aircraft.getAirline());
+
+		statuses = SelectChoices.from(Status.class, aircraft.getStatus());
+
+		dataset = super.unbindObject(aircraft, "model", "registrationNumber", "capacity", "cargoWeight", "status", "details");
+
+		dataset.put("airlines", selectedAirlines);
+		dataset.put("statuses", statuses);
+		dataset.put("confirmation", false);
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
