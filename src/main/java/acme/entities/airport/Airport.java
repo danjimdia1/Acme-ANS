c589de5036dcb958c872b9ab0338d899ac9f9977
mansangar13 <mansangar13@alum.us.es>
2025-06-24 19:44:01 +0200

    Arreglado los mensajes de error personalizados

--- src/main/java/acme/entities/airport/Airport.java
+++ src/main/java/acme/entities/airport/Airport.java
@@ -34,7 +34,7 @@ public class Airport extends AbstractEntity {
 	private String				name;
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{3}$")
+	@ValidString(pattern = "^[A-Z]{3}$", message = "{acme.validation.iataCode.message}")
 	@Column(unique = true)
 	private String				iataCode;
 
@@ -64,7 +64,7 @@ public class Airport extends AbstractEntity {
 	private String				email;
 
 	@Optional
-	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@ValidString(pattern = "^\\+?\\d{6,15}$", message = "{acme.validation.contactPhoneNumber.message}")
 	@Automapped
 	private String				contactPhoneNumber;
 
