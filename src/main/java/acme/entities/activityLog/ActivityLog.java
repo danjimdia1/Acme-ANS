2a12ab87013c60a8dba1a3bf3fa8b6780a11de48
manpervel2 <manuvez88@gmail.com>
2025-07-03 18:09:44 +0200

    Student#3-C2-Task-035

--- src/main/java/acme/entities/activityLog/ActivityLog.java
+++ src/main/java/acme/entities/activityLog/ActivityLog.java
@@ -4,7 +4,9 @@ package acme.entities.activityLog;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -22,6 +24,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "draftMode"), @Index(columnList = "registrationMoment")
+})
 public class ActivityLog extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
--- src/main/java/acme/realms/crewMember/CrewMember.java
+++ src/main/java/acme/realms/crewMember/CrewMember.java
@@ -3,7 +3,9 @@ package acme.realms.crewMember;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
@@ -23,6 +25,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @ValidCrewMember
+@Table(indexes = {
+	@Index(columnList = "availabilityStatus"), @Index(columnList = "salary_amount, salary_currency"), @Index(columnList = "languageSkills"), @Index(columnList = "yearsExperience")
+})
 public class CrewMember extends AbstractRole {
 
 	// Serialisation version --------------------------------------------------
